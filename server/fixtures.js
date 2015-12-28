03e8c7a8c8aec3e5cb103972739f30cac4142e44
邓佳佳 <2894220@qq.com>
2015-12-28 14:38:15 +0800

    [Maodou] Import modules data.

--- server/fixtures.js
+++ server/fixtures.js
@@ -3,41 +3,53 @@
 // ***************************************************************
 
 Meteor.startup(function () {
-  if (Modules.find().count() === 0) {
-    Modules.insert({icon: 'icon-c-core', title: 'Core'});
-    Modules.insert({icon: 'icon-c-core-plus', title: 'Core+'});
-    Modules.insert({icon: 'icon-c-usb-ttl', title: 'USBTTL'});
-    Modules.insert({icon: 'icon-c-usb-core', title: 'Core USB'});
-    Modules.insert({icon: 'icon-c-usb-host', title: 'USBHost'});
-    Modules.insert({icon: 'icon-c-wifi', title: 'WIFI'});
-    Modules.insert({icon: 'icon-c-2dot4g', title: '2.4G'});
-    Modules.insert({icon: 'icon-c-zig-bee', title: 'Zig Bee'});
-    Modules.insert({icon: 'icon-c-nfc', title: 'NFC'});
-    Modules.insert({icon: 'icon-c-wiz', title: 'WIZ'});
-    Modules.insert({icon: 'icon-c-enc', title: 'ENC'});
-    Modules.insert({icon: 'icon-c-rs485', title: 'RS485'});
-    Modules.insert({icon: 'icon-c-usb-ttl2', title: 'USBTTL'});
-    Modules.insert({icon: 'icon-c-gsm-gprs', title: 'GSM/GPRS'});
-    Modules.insert({icon: 'icon-c-bluetooth', title: 'Bluetooth'});
-    Modules.insert({icon: 'icon-c-battery', title: 'Battery'});
-    Modules.insert({icon: 'icon-c-gps', title: 'GPS'});
-    Modules.insert({icon: 'icon-c-crash', title: 'Crash'});
-    Modules.insert({icon: 'icon-c-tft-screen', title: 'TFT Screen'});
-    Modules.insert({icon: 'icon-c-oled-screen', title: 'OLED Screen'});
-    Modules.insert({icon: 'icon-c-10dof', title: '10DOF'});
-    Modules.insert({icon: 'icon-c-rtc', title: 'RTC'});
-    Modules.insert({icon: 'icon-c-micro-sd', title: 'MicroSD'});
-    Modules.insert({icon: 'icon-c-amplifier', title: 'Amplifier'});
-    Modules.insert({icon: 'icon-c-temperture', title: 'Temperture'});
-    Modules.insert({icon: 'icon-c-plush', title: 'Plush'});
-    Modules.insert({icon: 'icon-c-lightness', title: 'Lightness'});
-    Modules.insert({icon: 'icon-c-motor', title: 'Motor'});
-    Modules.insert({icon: 'icon-c-stepper', title: 'Stepper'});
-    Modules.insert({icon: 'icon-c-led', title: 'LED'});
-    Modules.insert({icon: 'icon-c-gray', title: 'Gray'});
-    Modules.insert({icon: 'icon-c-ir-receiver', title: 'IR Receiver'});
-    Modules.insert({icon: 'icon-c-ir-sensor', title: 'IR Sensor'});
-    Modules.insert({icon: 'icon-c-pir', title: 'PIR'});
-    Modules.insert({icon: 'icon-c-color-led', title: 'Color LED'});
-  }
+    var findIcon = Modules.findOne({icon: "icon-c-temperature"});
+    if (!findIcon) {
+        Modules.remove({});
+    }
+
+    if (Modules.find().count() === 0) {
+        ReactionCore.Log.info('Importing module data to collection modules ...');
+        var moduleList = [
+            {icon: 'icon-c-core', title: 'Core', favorite: [null], favcount: 0},
+            {icon: 'icon-c-core-plus', title: 'Core+', favorite: [null], favcount: 0},
+            {icon: 'icon-c-usb-ttl', title: 'USBTTL', favorite: [null], favcount: 0},
+            {icon: 'icon-c-usb-core', title: 'Core USB', favorite: [null], favcount: 0},
+            {icon: 'icon-c-usb-host', title: 'USBHost', favorite: [null], favcount: 0},
+            {icon: 'icon-c-wifi', title: 'WIFI', favorite: [null], favcount: 0},
+            {icon: 'icon-c-2dot4g', title: '2.4G', favorite: [null], favcount: 0},
+            {icon: 'icon-c-zig-bee', title: 'Zig Bee', favorite: [null], favcount: 0},
+            {icon: 'icon-c-nfc', title: 'NFC', favorite: [null], favcount: 0},
+            {icon: 'icon-c-wiz', title: 'WIZ', favorite: [null], favcount: 0},
+            {icon: 'icon-c-enc', title: 'ENC', favorite: [null], favcount: 0},
+            {icon: 'icon-c-rs485', title: 'RS485', favorite: [null], favcount: 0},
+            {icon: 'icon-c-usb-ttl2', title: 'USBTTL', favorite: [null], favcount: 0},
+            {icon: 'icon-c-gsm-gprs', title: 'GSM/GPRS', favorite: [null], favcount: 0},
+            {icon: 'icon-c-bluetooth', title: 'Bluetooth', favorite: [null], favcount: 0},
+            {icon: 'icon-c-battery', title: 'Battery', favorite: [null], favcount: 0},
+            {icon: 'icon-c-gps', title: 'GPS', favorite: [null], favcount: 0},
+            {icon: 'icon-c-crash', title: 'Crash', favorite: [null], favcount: 0},
+            {icon: 'icon-c-tft-screen', title: 'TFT Screen', favorite: [null], favcount: 0},
+            {icon: 'icon-c-oled-screen', title: 'OLED Screen', favorite: [null], favcount: 0},
+            {icon: 'icon-c-10dof', title: '10DOF', favorite: [null], favcount: 0},
+            {icon: 'icon-c-rtc', title: 'RTC', favorite: [null], favcount: 0},
+            {icon: 'icon-c-micro-sd', title: 'MicroSD', favorite: [null], favcount: 0},
+            {icon: 'icon-c-amplifier', title: 'Amplifier', favorite: [null], favcount: 0},
+            {icon: 'icon-c-temperature', title: 'Temperature', favorite: [null], favcount: 0},
+            {icon: 'icon-c-plush', title: 'Plush', favorite: [null], favcount: 0},
+            {icon: 'icon-c-lightness', title: 'Lightness', favorite: [null], favcount: 0},
+            {icon: 'icon-c-motor', title: 'Motor', favorite: [null], favcount: 0},
+            {icon: 'icon-c-stepper', title: 'Stepper', favorite: [null], favcount: 0},
+            {icon: 'icon-c-led', title: 'LED', favorite: [null], favcount: 0},
+            {icon: 'icon-c-gray', title: 'Gray', favorite: [null], favcount: 0},
+            {icon: 'icon-c-ir-receiver', title: 'IR Receiver', favorite: [null], favcount: 0},
+            {icon: 'icon-c-ir-sensor', title: 'IR Sensor', favorite: [null], favcount: 0},
+            {icon: 'icon-c-pir', title: 'PIR', favorite: [null], favcount: 0},
+            {icon: 'icon-c-color-led', title: 'Color LED', favorite: [null], favcount: 0}
+        ];
+
+        _.map(moduleList, function (item) {
+            Modules.insert(item);
+        });
+    }
 });
